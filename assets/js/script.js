9e1401e49b3d69ffd37dfb7347ddf5c099a9dc47
Amanda Morgan <AmandaC0022@gmail.com>
2021-07-24 09:04:23 -0500

    added a for loop to save each of the input datas

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,20 +12,73 @@ $(document).ready(function(){
     setInterval(update, 1000);
 }); 
 
-var time8AM = $("#time8AM"); 
-var input8AM = $("#input8AM"); 
-var save = $(".save"); 
+var allData = [
+    {
+        time: $("#time8AM"),
+        input: $("#input8AM"), 
+        lSKey:"Event 8AM", 
+    },
+    {
+        time: $("#time9AM"),
+        input: $("#input9AM"), 
+        lSKey:"Event 9AM",
+    },
+    {
+        time: $("#time10AM"),
+        input: $("#input10AM"), 
+        lSKey:"Event 10AM",
+    }, 
+    {
+        time: $("#time11AM"),
+        input: $("#input11AM"), 
+        lSKey:"Event 11AM",
+    }, 
+    {
+        time: $("#time12PM"),
+        input: $("#input12PM"), 
+        lSKey:"Event 12PM",
+    }, 
+    {
+        time: $("#time1PM"),
+        input: $("#input1PM"), 
+        lSKey:"Event 1PM",
+    }, 
+    {
+        time: $("#time2PM"),
+        input: $("#input2PM"), 
+        lSKey:"Event 2PM",
+    }, 
+    {
+        time: $("#time3PM"),
+        input: $("#input3PM"), 
+        lSKey:"Event 3PM",
+    }, 
+    {
+        time: $("#time4PM"),
+        input: $("#input4PM"), 
+        lSKey:"Event 4PM",
+    }, 
+    {
+        time: $("#time5PM"),
+        input: $("#input5PM"), 
+        lSKey:"Event 5PM",
+    }
+]; 
 
 // Saves User Input for first row to Local Storage  This works! Don't mess with it! 
-$(".save").on("click", function() {  
+$(".save").on("click", function() { 
+    for(var i=0; i<= allData.length; i++) {
+        localStorage.setItem(allData[i].lSKey, allData[i].input.val());
+    } 
+}) 
 
-    localStorage.setItem("Event 8AM", input8AM.val()); 
-})
+for (var i=0; i<=allData.length; i++) {
+    var newEvent = localStorage.getItem(allData[i].lSKey);
+}
 
+console.log(newEvent); 
 //user input is saved within the table 
-var newEvent8AM = localStorage.getItem("Event 8AM");
-
-$("#input8AM").attr("placeholder", newEvent8AM); 
+allData[0].input.attr("placeholder", newEvent8AM);
 
 //link current time to time table column
 
