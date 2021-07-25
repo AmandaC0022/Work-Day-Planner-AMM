7f6a7600c007ecd8a0d5bf328e67245420c6879b
Amanda Morgan <AmandaC0022@gmail.com>
2021-07-25 12:28:57 -0500

    solved all error messages

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,173 +12,102 @@ $(document).ready(function(){
     setInterval(update, 1000);
 }); 
 
-//holds all of selectors for the table 
-var allData = [
-    {
-        time: $("#time8AM"),
-        input: $("#input8AM"), 
-        lSKey:"Event 8AM", 
-        save: $("#save8AM"),
-    },
-    {
-        time: $("#time9AM"),
-        input: $("#input9AM"), 
-        lSKey:"Event 9AM",
-        save: $("#save9AM"),
-    },
-    {
-        time: $("#time10AM"),
-        input: $("#input10AM"), 
-        lSKey:"Event 10AM",
-        save: $("#save10AM"),
-    }, 
-    {
-        time: $("#time11AM"),
-        input: $("#input11AM"), 
-        lSKey:"Event 11AM",
-        save: $("#save11AM"),
-    }, 
-    {
-        time: $("#time12PM"),
-        input: $("#input12PM"), 
-        lSKey:"Event 12PM",
-        save: $("#save12PM"),
-    }, 
-    {
-        time: $("#time1PM"),
-        input: $("#input1PM"), 
-        lSKey:"Event 1PM",
-        save: $("#save1PM"),
-    }, 
-    {
-        time: $("#time2PM"),
-        input: $("#input2PM"), 
-        lSKey:"Event 2PM",
-        save: $("#save2PM"),
-    }, 
-    {
-        time: $("#time3PM"),
-        input: $("#input3PM"), 
-        lSKey:"Event 3PM",
-        save: $("#save3PM"),
-    }, 
-    {
-        time: $("#time4PM"),
-        input: $("#input4PM"), 
-        lSKey:"Event 4PM",
-        save: $("#save4PM"),
-    }, 
-    {
-        time: $("#time5PM"),
-        input: $("#input5PM"), 
-        lSKey:"Event 5PM",
-        save: $("#save5PM"),
-    }
-]; 
+//global arrays with links to table 
+var inputIDs = [$("#input8AM"), $("#input9AM"), $("#input10AM"), $("#input11AM"), $("#input12PM"), $("#input1PM"), $("#input2PM"), $("#input3PM"), $("#input4PM"), $("#input5PM")]; 
 
-// Saves User Input for first row to Local Storage  Does weird things when I save all of them... It writes over the old placeholders when I save a new one.  
+var lsKeys = ["Event 8AM", "Event 9AM", "Event 10AM", "Event 11AM", "Event 12PM", "Event 1PM", "Event 2PM", "Event 3PM", "Event 4PM", "Event 5PM"]; 
 
-// $(".save").on("click", function() { 
-//     for(var i=0; i<= allData.length; i++) {
-//         localStorage.setItem(allData[i].lSKey, allData[i].input.val());
-//     } 
-// }) 
+var saves = [$("#save8AM"), $("#save9AM"), $("#save10AM"), $("#save11AM"), $("#save12PM"), $("#save1PM"), $("#save2PM"), $("#save3PM"), $("#save4PM"), $("#save5PM")]; 
 
-//There has to be a way to minimize this code... research forEach function 
-
-// When I put this in a for loop, it constantly writes over all of the local storage Items. 
-
-allData[0].save.on("click", function(){
-    localStorage.setItem(allData[0].lSKey, allData[0].input.val()); 
+//When I click the save button, I am setting the input value to local storage 
+saves[0].on("click", function(){
+    localStorage.setItem(lsKeys[0], inputIDs[0].val());
+})
+saves[1].on("click", function(){
+    localStorage.setItem(lsKeys[1], inputIDs[1].val());
+})
+saves[2].on("click", function(){
+    localStorage.setItem(lsKeys[2], inputIDs[2].val());
+})
+saves[3].on("click", function(){
+    localStorage.setItem(lsKeys[3], inputIDs[3].val());
+})
+saves[4].on("click", function(){
+    localStorage.setItem(lsKeys[4], inputIDs[4].val());
+})
+saves[5].on("click", function(){
+    localStorage.setItem(lsKeys[5], inputIDs[5].val());
+})
+saves[6].on("click", function(){
+    localStorage.setItem(lsKeys[6], inputIDs[6].val());
+})
+saves[7].on("click", function(){
+    localStorage.setItem(lsKeys[7], inputIDs[7].val());
+})
+saves[8].on("click", function(){
+    localStorage.setItem(lsKeys[8], inputIDs[8].val());
+})
+saves[9].on("click", function(){
+    localStorage.setItem(lsKeys[9], inputIDs[9].val());
 })
 
-allData[1].save.on("click", function() {
-    localStorage.setItem(allData[1].lSKey, allData[1].input.val()); 
-})
-
-//grabs all the set values from local storage 
-var event8AM = localStorage.getItem(allData[0].lSKey); 
-var event9AM = localStorage.getItem(allData[1].lSKey); 
-var event10AM = localStorage.getItem(allData[2].lSKey); 
-var event11AM = localStorage.getItem(allData[3].lSKey); 
-var event12PM = localStorage.getItem(allData[4].lSKey); 
-var event1PM = localStorage.getItem(allData[5].lSKey); 
-var event2PM = localStorage.getItem(allData[6].lSKey); 
-var event3PM = localStorage.getItem(allData[7].lSKey); 
-var event4PM = localStorage.getItem(allData[8].lSKey); 
-var event5PM = localStorage.getItem(allData[9].lSKey); 
-
-//user input is saved within the table 
-allData[0].input.attr("placeholder", event8AM);
-allData[1].input.attr("placeholder", event9AM);
-allData[2].input.attr("placeholder", event10AM);
-allData[3].input.attr("placeholder", event11AM);
-allData[4].input.attr("placeholder", event12PM);
-allData[5].input.attr("placeholder", event1PM);
-allData[6].input.attr("placeholder", event2PM);
-allData[7].input.attr("placeholder", event3PM);
-allData[8].input.attr("placeholder", event4PM);
-allData[9].input.attr("placeholder", event5PM);
-
-//link current time to time table column
+//Displays all placeholder text from what is stored on local storage 
+inputIDs[0].attr("placeholder", localStorage.getItem(lsKeys[0]));
+inputIDs[1].attr("placeholder", localStorage.getItem(lsKeys[1]));
+inputIDs[2].attr("placeholder", localStorage.getItem(lsKeys[2]));
+inputIDs[3].attr("placeholder", localStorage.getItem(lsKeys[3]));
+inputIDs[4].attr("placeholder", localStorage.getItem(lsKeys[4]));
+inputIDs[5].attr("placeholder", localStorage.getItem(lsKeys[5]));
+inputIDs[6].attr("placeholder", localStorage.getItem(lsKeys[6]));
+inputIDs[7].attr("placeholder", localStorage.getItem(lsKeys[7]));
+inputIDs[8].attr("placeholder", localStorage.getItem(lsKeys[8])); 
+inputIDs[9].attr("placeholder", localStorage.getItem(lsKeys[9]));
 
 // This displays the current hour 
 var hour = moment().hour(); 
-console.log(hour); 
 
-var timeIDs = [$("#time8AM"), $("#time9AM"), $("#time10AM"), $("#time11AM"), $("#time12PM"), $("#time1PM"), $("#time2PM"), $("#time3PM"), $("#time4PM"), $("#time5PM")]; 
-var arr = timeIDs.slice(2);
-arr.forEach(element => element.addClass("futureTime")); 
+//this saves all of the timeIDs in one place 
+var timeIDs = [$("#time8AM"), $("#time9AM"), $("#time10AM"), $("#time11AM"), $("#time12PM"), $("#time1PM"), $("#time2PM"), $("#time3PM"), $("#time4PM"), $("#time5PM")];  
 
+//this function controls the background colors of the table 
 function changeBackground() {
     if (hour === 8) {
        timeIDs[0].addClass("currentTime");
        var arr = timeIDs.slice(1);
        arr.forEach(element => element.addClass("futureTime"));
-    }
-    if (hour === 9) {
+    } else if (hour === 9) {
         timeIDs[1].addClass("currentTime"); 
         var arr = timeIDs.slice(2);
         arr.forEach(element => element.addClass("futureTime"));
-    }
-    if (hour === 10) {
+    } else if (hour === 10) {
         timeIDs[2].addClass("currentTime"); 
-        console.log(timeIDs[2]);
         var arr = timeIDs.slice(3);
-        console.log(arr); 
         arr.forEach(element => element.addClass("futureTime"));
-    }
-    if (hour === 11) {
+    } else if (hour === 11) {
         timeIDs[3].addClass("currentTime"); 
         var arr = timeIDs.slice(4);
         arr.forEach(element => element.addClass("futureTime"));
-    }
-    if (hour === 12) {
+    } else if (hour === 12) {
         timeIDs[4].addClass("currentTime");
         var arr = timeIDs.slice(5);
         arr.forEach(element => element.addClass("futureTime")); 
-    }
-    if (hour === 1) {
+    } else if (hour === 1) {
         timeIDs[5].addClass("currentTime"); 
         var arr = timeIDs.slice(6);
         arr.forEach(element => element.addClass("futureTime"));
-    }
-    if (hour === 2) {
+    } else if (hour === 2) {
         timeIDs[6].addClass("currentTime"); 
         var arr = timeIDs.slice(7);
         arr.forEach(element => element.addClass("futureTime"));
-    }
-    if (hour === 3) {
+    } else if (hour === 3) {
         timeIDs[7].addClass("currentTime"); 
         var arr = timeIDs.slice(8);
         arr.forEach(element => element.addClass("futureTime"));
-    }
-    if (hour === 4) {
+    } else if (hour === 4) {
         timeIDs[8].addClass("currentTime"); 
         var arr = timeIDs.slice(9);
         arr.forEach(element => element.addClass("futureTime"));
-    }
-    if (hour === 5) {
+    } else {
         timeIDs[9].addClass("currentTime"); 
     }
 }; 
